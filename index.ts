2d778ee6b041ac86233eb78e1618992c0f32f4cd
le Duc Thien <ducthien09092005@gmail.com>
2026-04-21 14:03:09 +0700

    cofig CORS

--- index.ts
+++ index.ts
@@ -1,18 +1,26 @@
-import express,{Express} from "express";
+import express, { Express } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import * as database from "./config/database";
 database.connect();
 const app: Express = express();
 const port: number | string = process.env.PORT || 5000;
+// CORS
+// const corsOption = [
+//     origin: 'http://example.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// ];
+// app.use(cors(corsOption));
+app.use(cors());
 // body-parser
 // const bodyParser = require("body-parser");
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // Router
 import { mainRouterV1 } from "./api/v1/router/index.router";
 mainRouterV1(app);
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
