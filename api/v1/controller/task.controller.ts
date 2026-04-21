c3faca23be2218be3399161c9d9db6c68331fdf1
le Duc Thien <ducthien09092005@gmail.com>
2026-04-21 11:23:45 +0700

    filter by status

--- api/v1/controller/task.controller.ts
+++ api/v1/controller/task.controller.ts
@@ -2,10 +2,18 @@ import { Request, Response } from "express";
 import Task from "../model/task.model";
 
 export const index = async (req: Request, res: Response)=>{
-    const task = await Task.find({
+    interface Find {
+        deleted: boolean,
+        status?: string
+    }
+    const find: Find = {
         deleted: false
-    });
-    console.log(task);
+    }
+    if(req.query.status){
+        find.status= req.query.status.toString();
+    }
+    const task = await Task.find(find);
+    
     res.json(task);
 }
 
